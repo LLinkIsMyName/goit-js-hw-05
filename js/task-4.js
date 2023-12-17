dc0aedc30f2758e95035a9e1d0c4d1d07ecf3a61
LLinkIsMyName <144070593+LLinkIsMyName@users.noreply.github.com>
2023-12-17 20:23:17 +0200

    upt

--- js/task-4.js
+++ js/task-4.js
@@ -1,4 +1,4 @@
-const getTotalBalanceByGender = (users, gender) =>
+let getTotalBalanceByGender = (users, gender) =>
     users
         .filter(user => user.gender === gender)
         .reduce((totalBalance, user) => totalBalance + user.balance, 0);
